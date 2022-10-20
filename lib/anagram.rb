9ee67dca6581c4fe68ca7b478a1591cbbac913a0
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-10-20 13:34:42 +0300

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+#anagram
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    
+    def match (possible_anagram_array)
+        possible_anagram_array.filter do |element|
+            element.chars.sort == word.chars.sort 
+        end
+    end
+end
+
